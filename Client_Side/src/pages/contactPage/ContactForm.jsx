4551f50baf55e3580621ed6bad0ec602472d029d
ElishevaSegal <139677575+ElishevaSegal@users.noreply.github.com>
2024-04-10 09:58:14 +0300

    updated

--- Client_Side/src/pages/contactPage/ContactForm.jsx
+++ Client_Side/src/pages/contactPage/ContactForm.jsx
@@ -33,7 +33,7 @@ const ContactForm = () => {
         email: inputsValue.email,
         message: inputsValue.message,
       };
-
+      const { data } = await axios.post("/contact", request);
       toast("We got your message, thank you :)", {
         position: "top-center",
         autoClose: 5000,
